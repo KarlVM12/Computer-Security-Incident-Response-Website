da9dd791171d014054b0154b54d89338444c786e
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-12-16 19:52:21 -0500

    FINAL CHANGES TO SQL CODE
    Needed to fix normalization errors

--- Website/final project script.sql
+++ Website/final project script.sql
@@ -1,4 +1,4 @@
-#DROP DATABASE csirt;
+DROP DATABASE csirt;
 CREATE DATABASE csirt;
 USE csirt;
 
@@ -18,23 +18,20 @@ CREATE TABLE INCIDENT (
     CONSTRAINT Incident_PK PRIMARY KEY (incidentID),
     CONSTRAINT Incident_FK1 FOREIGN KEY (handlerName)
 		REFERENCES HANDLER (handlerName)
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE PERSON (
-    associationID INTEGER NOT NULL,
-    incidentID INTEGER,
+    associationID INTEGER AUTO_INCREMENT NOT NULL,
     lastName VARCHAR(25),
     firstName VARCHAR(25),
     jobTitle VARCHAR(50),
     emailAddress VARCHAR(100),
-    CONSTRAINT Person_PK PRIMARY KEY (associationID),
-    CONSTRAINT Person_FK1 FOREIGN KEY (incidentID)
-        REFERENCES INCIDENT (incidentID)
-        ON DELETE CASCADE
+    CONSTRAINT Person_PK PRIMARY KEY (associationID)
 );
 
 CREATE TABLE COMMENT (
-    commentID INTEGER NOT NULL,
+    commentID INTEGER NOT NULL AUTO_INCREMENT,
     incidentID INTEGER NOT NULL,
     commentDescription VARCHAR(250),
     commentDate DATE,
@@ -58,6 +55,7 @@ CREATE TABLE IPADDRESS (
         REFERENCES INCIDENT (incidentID)
         ON DELETE CASCADE
 );
+
 ALTER TABLE INCIDENT
 ADD CONSTRAINT Incident_FK2 FOREIGN KEY (associationID) 
 	REFERENCES PERSON (associationID);
@@ -69,13 +67,21 @@ INSERT INTO HANDLER VALUES (
 	'John Madoff');
 INSERT INTO HANDLER VALUES (
 	'Tim Apple');
+INSERT INTO HANDLER VALUES (
+    'Bill Murray');
     
 INSERT INTO PERSON VALUES (
-	7654, NULL, NULL, NULL, NULL, NULL);
+    7654, NULL, NULL, NULL, NULL);
 INSERT INTO PERSON VALUES (
-	3456, NULL, NULL, NULL, NULL, NULL);
+	3456, NULL, NULL, NULL, NULL);
 INSERT INTO PERSON VALUES (
-	6789, NULL, NULL, NULL, NULL, NULL);
+	6789, NULL, NULL, NULL, NULL);
+INSERT INTO PERSON VALUES ( 
+    9001, NULL, NULL, NULL, NULL);
+INSERT INTO PERSON VALUES ( 
+    9987, NULL, NULL, NULL, NULL);
+INSERT INTO PERSON VALUES ( 
+    8901, NULL, NULL, NULL, NULL);
 
 INSERT INTO INCIDENT VALUES (
 	222903, 'Malware', '2020-09-18', 'open', 'Computer Lab 12 Infected', 'Steve Woz', null);
@@ -85,27 +91,50 @@ INSERT INTO INCIDENT VALUES (
 	543267, 'Hardware Malfunction', '2018-11-20', 'closed', 'Angie\'s Dropped Laptop', 'Tim Apple', 3456);
 INSERT INTO INCIDENT VALUES (
 	726818, 'Theft of Property', '2020-12-20', 'closed', 'Frank\'s Laptop Not Returned in Time', 'John Madoff', 6789); 
+INSERT INTO INCIDENT VALUES (
+    894933, 'Damaged Laptop', '2020-12-06', 'closed', 'Rosa\'s Laptop screen broken', 'Bill Murray', 9001);
+INSERT INTO INCIDENT VALUES (
+    564789, 'Malware', '2020-03-02', 'closed', 'Tim infected his laptop with malware', 'Bill Murray', 7654);
+INSERT INTO INCIDENT VALUES (
+    009231, 'Software Update', '2020-12-20','open', 'Need to update Kurt\'s software', 'Tim Apple', 9987);
+INSERT INTO INCIDENT VALUES (
+	972132, 'Theft of Property', '2019-11-20', 'closed', 'Needs to return borrowed iPad', 'John Madoff', 8901);
+    
 
 UPDATE PERSON
-SET incidentID = 222398, lastName = 'Claude', firstName = 'Tim', jobTitle = 'Accountant', emailAddress = 'timclaude45@gmail.com'
+SET lastName = 'Claude', firstName = 'Tim', jobTitle = 'Accountant', emailAddress = 'timclaude45@gmail.com'
 WHERE associationID = 7654;
 
 UPDATE PERSON
-SET incidentID = 543267, lastName = 'Rivera', firstName = 'Angie', jobTitle = 'Secretary', emailAddress = 'angierivera23@gmail.com'
+SET lastName = 'Rivera', firstName = 'Angie', jobTitle = 'Secretary', emailAddress = 'angierivera23@gmail.com'
 WHERE associationID = 3456;
 
 UPDATE PERSON
-SET incidentID = 726818, lastName = 'Clinton', firstName = 'Frank', jobTitle = 'Developer', emailAddress = 'frankcliton12@gmail.com'
+SET lastName = 'Clinton', firstName = 'Frank', jobTitle = 'Developer', emailAddress = 'frankcliton12@gmail.com'
 WHERE associationID = 6789;
+
+UPDATE PERSON
+SET lastName = 'Muller', firstName = 'Rosa', jobTitle = 'Developer', emailAddress = 'rosamuller21@gmail.com'
+WHERE associationID = 9001;
+
+UPDATE PERSON
+SET lastName = 'Muller', firstName = 'Kurt', jobTitle = 'Designer', emailAddress = 'kurtmuller123@gmail.com'
+WHERE associationID = 9987;
+
+UPDATE PERSON
+SET lastName = 'Bob', firstName = 'Jim', jobTitle = 'Dingus', emailAddress = 'jimbob67@gmail.com'
+WHERE associationID = 8901;
     
 INSERT INTO COMMENT VALUES (
-	213433, 222903, 'The entire lab was infected with malware that encrypted the hard drives of each computer', '2020-09-20', 'Steve Woz');
+	null, 222903, 'The entire lab was infected with malware that encrypted the hard drives of each computer', '2020-09-20', 'Steve Woz');
 INSERT INTO COMMENT VALUES (
-	434411, 222398, 'Tim\'s laptop hard drive has been corrupted. Probably needs a fresh OS install', '2019-11-02', 'John Madoff');
+	null, 222398, 'Tim\'s laptop hard drive has been corrupted. Probably needs a fresh OS install', '2019-11-02', 'John Madoff');
 INSERT INTO COMMENT VALUES (
-	878987, 543267, 'Angie has dropped her laptop, breaking it. Needs a replacement', '2018-11-20', 'Tim Apple');
+	null, 543267, 'Angie has dropped her laptop, breaking it. Needs a replacement', '2018-11-20', 'Tim Apple');
 INSERT INTO COMMENT VALUES (
-	878988, 543267, 'Angie has been given a new laptop. Issue is closed.', '2018-11-27', 'Tim Apple');
+	null, 543267, 'Angie has been given a new laptop. Issue is closed.', '2018-11-27', 'Tim Apple');
+INSERT INTO COMMENT VALUES (
+	null, 009231, 'He needs the latest year updates for Premier Pro', '2020-12-21', 'Tim Apple');
 
 INSERT INTO IPADDRESS VALUES (
 	7654, '145.10.34.3', 222398);
@@ -113,20 +142,26 @@ INSERT INTO IPADDRESS VALUES (
 	3456, '127.255.255.255', 543267);
 INSERT INTO IPADDRESS VALUES (
 	6789, '191.255.130.332', 726818);
+INSERT INTO IPADDRESS VALUES (
+	8901, '876.232.130.332', 972132);
     
-#SELECT 
-#    *
-#FROM
-#    INCIDENT;
-#SELECT 
-#    *
-#FROM
-#    PERSON;
-#SELECT 
-#    *
-#FROM
-#    COMMENT;
-#SELECT 
-#    *
-#FROM
-#    IPADDRESS;
+SELECT 
+    *
+FROM
+    INCIDENT;
+SELECT 
+    *
+FROM
+    PERSON;
+SELECT 
+    *
+FROM
+    COMMENT;
+SELECT 
+    *
+FROM
+    IPADDRESS;
+SELECT 
+    *
+FROM
+    HANDLER;
