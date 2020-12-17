b99cd9468ad6d8d3a86949d88981ee79b62983f3
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-12-17 16:00:06 -0500

    Update RemovePeople.php

--- Website/RemovePeople.php
+++ Website/RemovePeople.php
@@ -26,7 +26,7 @@
                 die("connection failed: " . $conn->connect_error ."<br>");
             } 
             
-            $sql = "DELETE FROM INVOLVEDPERSON WHERE associationID = ".$_POST["associationID"] AND incidentID = ".$POST["incidentID"]";
+            $sql = "DELETE FROM INVOLVEDPERSON WHERE associationID = ".$_POST["associationID"]" AND incidentID = ".$POST["incidentID"]";
            
             $result = $conn->query($sql);
             
