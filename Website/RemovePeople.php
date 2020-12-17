914b52adf92f82664b006780750e860e36bdaa97
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-12-17 14:55:54 -0500

    update: added button to go back to main page

--- Website/RemovePeople.php
+++ Website/RemovePeople.php
@@ -33,6 +33,10 @@
             $conn->close();
         ?>
         <p>Removed!</p>
-
+        
+        <form action="IncidentReports.html"> 
+            <input type="submit" value="Go Back to Main Page"><br>
+        </form>
+        
     </body>
 </html>
