d605516ec0b574390001d011550363dd158d97a8
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-12-17 14:47:56 -0500

    Fixed an error in PHP

--- Website/CreatedReport.php
+++ Website/CreatedReport.php
@@ -30,7 +30,7 @@
             
             $sql2 = "INSERT INTO COMMENT VALUES (null,".$_POST["incidentID"].",'".$_POST["commentDescription"]."','".$_POST["commentDate"]."','".$_POST["handlerName"]."')";
 
-            #sql3 = 'INSERT INTO PERSON VALUES (".$_POST["associationID"].",'".$_POST["lastName"]."','".$_POST["firstName"]."','".$_POST["jobTitle"]."','".$_POST["emailAddress"]."')";
+            $sql3 = "INSERT INTO PERSON VALUES (".$_POST["associationID"].",'".$_POST["lastName"]."','".$_POST["firstName"]."','".$_POST["jobTitle"]."','".$_POST["emailAddress"]."')";
 
             $sql4 = "INSERT INTO IPADDRESS VALUES (".$_POST["associationID"].",'".$_POST["IPAddress"]."',".$_POST["incidentID"].")";
 
