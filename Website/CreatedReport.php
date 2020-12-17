9c75bca0fabe2e7b5ac7c718d16fc868b0d301b2
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-12-17 14:48:47 -0500

    Added Button to go back to main page

--- Website/CreatedReport.php
+++ Website/CreatedReport.php
@@ -45,6 +45,9 @@
             $conn->close();
         ?>
         <p>Added!</p>
-
+        
+        <form action="IncidentReports.html"> 
+            <input type="submit" value="Go Back to Main Page"><br>
+        </form>
     </body>
 </html>
