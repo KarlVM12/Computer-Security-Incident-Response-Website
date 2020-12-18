8005a69ce6110a231f26559f3f3cb1bf59ada7ad
kurtmuller-studnet <kurtmuller@mail.adelphi.edu>
2020-12-18 10:32:49 -0500

    update: Fixed Createdreport.php and NewReport.html

--- Website/CreatedReport.php
+++ Website/CreatedReport.php
@@ -19,7 +19,7 @@
             
             // Create connection
             // MySQLi is Object-Oriented method
-            $conn = new sqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
             
             // Check connection
             if ($conn->connect_error) {
