c8ebfafc47c27fff6980aa24890319ad4c145cf0
kurtmuller-studnet <kurtmuller@mail.adelphi.edu>
2020-12-17 20:12:22 -0500

    Update SendingEmail.php

--- Website/SendingEmail.php
+++ Website/SendingEmail.php
@@ -5,7 +5,7 @@
       <title>Sending Email</title>
    </head>
    <body>
-      
+      <h3>Emaling</h3>
       <?php
          $servername = "localhost";
          $username = "root"; // Mysql username
@@ -22,25 +22,31 @@
                die("connection failed: " . $conn->connect_error ."<br>");
          } 
          
-         $sql = "SELECT * FROM INCIDENT WHERE incidentID = ".$_POST["incidentID"].";";
-         $sql .= "SELECT * FROM INVOLVEDPERSON WHERE incidentID = ".$_POST["incidentID"].";";
-         $sql .= "SELECT * FROM COMMENT WHERE incidentID = ".$_POST["incidentID"].";";
-         $sql .= "SELECT * FROM IPADDRESS WHERE incidentID = ".$_POST["incidentID"];
+         $sql1 = "SELECT * FROM INCIDENT WHERE incidentID = ".$_POST["incidentID"];
+         $sql2 = "SELECT * FROM INVOLVEDPERSON WHERE incidentID = ".$_POST["incidentID"];
+         $sql3 = "SELECT * FROM COMMENT WHERE incidentID = ".$_POST["incidentID"];
+         $sql4 = "SELECT * FROM IPADDRESS WHERE incidentID = ".$_POST["incidentID"];
          
-         $result = $conn->query($sql);
-	      echo $result;
+         $result = $conn->query($sql1);
+         $result = $conn->query($sql2);
+         $result = $conn->query($sql3);
+         $result = $conn->query($sql4);
+
+	      echo "'".$result."'";
       
          $email_from = "'".$_POST["sendingemail"]."'";
          $email_subject = "CSIRT Report Email";
-         $email_body = "You have received a new message from the user ".$_POST["name"].".\n".
-         $email_body .= "Here is the message:\n ".$_POST["message"]."\n".
-         $email_body .= "$result\n";
+         $email_body = "You have received a new message from the user '".$_POST["name"]."'\n".
+         $email_body .= "Here is the message:\n '".$_POST["message"]."'\n".
+         $email_body .= "'".$result."\n'";
          
-         $to = "$email_from";
-         $headers = "From: $email_from \r\n";
+         $to = "'".$email_from."'";
+         $headers = "From: '".$email_from."' \r\n";
          $headers .= "Reply-To: $email_from \r\n";
          mail($to,$email_subject,$email_body,$headers);
+
+         $conn->close();
       ?>
-      
+      <p>Emailed!</p>
    </body>
 </html>
