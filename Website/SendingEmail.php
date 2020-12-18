680be89553329401c4f8dd9bfe2b274bc7c67287
kurtmuller-studnet <kurtmuller@mail.adelphi.edu>
2020-12-17 20:49:25 -0500

    Update SendingEmail.php

--- Website/SendingEmail.php
+++ Website/SendingEmail.php
@@ -32,15 +32,27 @@
          $result = $conn->query($sql3);
          $result = $conn->query($sql4);
 
-         echo "<table> <tr> <th>incidentID</th>  <th>incidentType</th> <th>creationDate</th> <th>incidentState</th> <th>incidentName</th> <th>handlerName</th> </tr>".
+         $row = $result->fetch_assoc();
+
+         $sqlresults = "<h4>Incident</h4><br><table> <tr> <th>incidentID</th>  <th>incidentType</th> <th>creationDate</th> <th>incidentState</th> <th>incidentName</th> <th>handlerName</th> </tr>".
                "<tr><td>".$row["incidentID"]."</td>"."<td>".$row["incidentType"]."</td>"."<td>".$row["creationDate"]."</td>"."<td>".$row["incidentState"]."</td>"."<td>".$row["incidentName"]."</td>"."<td>".$row["handlerName"]."</td></tr>"."</table>";
+
+         $sqlresults .= "\n<h4>Involved People</h4><br><table> <tr> <th>associationID</th> <th>incidentID</th> </tr>".
+               "<tr><td>".$row["associationID"]."</td>"."<td>".$row['incidentID']."</td></tr>"."<table>";
+        
+         $sqlresults .= "\n<h4>Comments</h4><br><table> <tr> <th>commentID</th> <th>incidentID</th> <th>commentDescription</th> <th>commentDate</th> <th>handlerName</th> </tr>".
+               "<tr><td>".$row["commentID"]."</td>"."<td>".$row['incidentID']."</td>"."<td>".$row['commentDescription']."</td>"."<td>".$row['commentDate']."</td>"."<td>".$row['handlerName']."</td></tr>"."</table>";
+         
+         $sqlresults .= "\n<h4>IP Address</h4><br><table> <tr> <th>associationID</th> <th>IPAddress</th> <th>incidentID</th> </tr>".
+               "<tr><td>".$row['associationID']."</td>"."<td>".$row['IPAddress']."</td>"."<td>".$row['incidentID']."</td></tr>"."</table>";
       
+         echo $sqlresults;
       
          $email_from = "'".$_POST["sendingemail"]."'";
          $email_subject = "CSIRT Report Email";
          $email_body = "You have received a new message from the user '".$_POST["name"]."'\n".
          $email_body .= "Here is the message:\n '".$_POST["message"]."'\n".
-         $email_body .= "'".$result."\n'";
+         $email_body .= "'".$sqlresults."'\n";
          
          $to = "'".$email_from."'";
          $headers = "From: '".$email_from."' \r\n";
